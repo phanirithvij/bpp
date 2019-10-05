298fb77ddb85dea2ff39cbbcfdd4d62ea9c5e106
phanirithvij <phanirithvij2000@gmail.com>
2019-10-05 17:45:33 +0530

    hpp

--- main.cc
+++ main.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "main.h"
+#include "main.hpp"
 
 using namespace std;
 
--- utils.cc
+++ utils.cc
@@ -1,16 +1,17 @@
-#include "main.h"
+#include "main.hpp"
 
 #ifdef _WIN32
 #include <experimental/filesystem>
-bool file_exists(const std::string& name)
+bool file_exists(const std::string &name)
 {
-    return std::experimental::filesystem::exists(name);
+	return std::experimental::filesystem::exists(name);
 }
 #else
 #include <sys/stat.h>
 #include <unistd.h>
-bool file_exists (const std::string& name) {
-  struct stat buffer;   
-  return (stat (name.c_str(), &buffer) == 0); 
+bool file_exists(const std::string &name)
+{
+	struct stat buffer;
+	return (stat(name.c_str(), &buffer) == 0);
 }
 #endif
